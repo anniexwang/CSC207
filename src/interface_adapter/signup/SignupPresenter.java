905b79b996ce0b43f15de5e66886c08037abdf58
Allen <you@example.com>
2023-11-13 12:19:41 -0500

    Added signup view and started on login view

--- src/interface_adapter/signup/SignupPresenter.java
+++ src/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,44 @@
 package interface_adapter.signup;
 
-public class SignupPresenter {
+import use_case.SignupOutputBoundary;
+import use_case.SignupOutputData;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SignupPresenter implements SignupOutputBoundary {
+
+    private final interface_adapter.SignupViewModel signupViewModel;
+    private final LoginViewModel loginViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           interface_adapter.SignupViewModel signupViewModel,
+                           LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        // On success, switch to the login view.
+        LocalDateTime responseTime = LocalDateTime.parse(response.getCreationTime());
+        response.setCreationTime(responseTime.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+
+        interface_adapter.SignupState signupState = signupViewModel.getState();
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(response.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        interface_adapter.SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(error);
+        signupViewModel.firePropertyChanged();
+    }
 }
