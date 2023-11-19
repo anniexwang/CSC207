07475cdd4df81c436f1159ad1502c2ba9530f69e
Allen <you@example.com>
2023-11-19 14:15:03 -0500

    FIGHT THE POWAH

--- src/app/Main.java
+++ src/app/Main.java
@@ -14,55 +14,74 @@ import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        // Build the main program window, the main panel containing the
-        // various cards, and the layout, and stitch them together.
-
-        // The main application window.
-        JFrame application = new JFrame("Translayte");
-        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        CardLayout cardLayout = new CardLayout();
-
-        // The various View objects. Only one view is visible at a time.
-        JPanel views = new JPanel(cardLayout);
-        application.add(views);
-
-        // This keeps track of and manages which view is currently showing.
-        ViewManagerModel viewManagerModel = new ViewManagerModel();
-        new ViewManager(views, cardLayout, viewManagerModel);
-
-        // The data for the views, such as username and password, are in the ViewModels.
-        // This information will be changed by a presenter object that is reporting the
-        // results from the use case. The ViewModels are observable, and will
-        // be observed by the Views.
-        LoginViewModel loginViewModel = new LoginViewModel();
-        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
-        SignupViewModel signupViewModel = new SignupViewModel();
-
-        FileUserDataAccessObject userDataAccessObject;
         try {
-            userDataAccessObject = new FileUserDataAccessObject("./users.csv", new CommonUserFactory());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            // Load the custom Goblin font
+                Font goblinFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/view/cc.TTF")).deriveFont(12f); // Adjust the font size as needed
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(goblinFont);
+
+            // Main application window setup
+            JFrame application = new JFrame("Translayte");
+            application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            // Card layout for switching between views
+            CardLayout cardLayout = new CardLayout();
+            JPanel views = new JPanel(cardLayout);
+            application.add(views);
+
+            // View manager model
+            ViewManagerModel viewManagerModel = new ViewManagerModel();
+            new ViewManager(views, cardLayout, viewManagerModel);
+
+            // ViewModels setup
+            LoginViewModel loginViewModel = new LoginViewModel();
+            LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+            SignupViewModel signupViewModel = new SignupViewModel();
+
+            // Data access object
+            FileUserDataAccessObject userDataAccessObject;
+            try {
+                userDataAccessObject = new FileUserDataAccessObject("./users.csv", new CommonUserFactory());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Creating views
+            SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject);
+            applyFontToComponent(signupView, goblinFont);
+            views.add(signupView, signupView.viewName);
+
+            LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel, loggedInViewModel, signupViewModel, userDataAccessObject);
+            applyFontToComponent(loginView, goblinFont);
+            views.add(loginView, loginView.viewName);
+
+            LoggedInView loggedInView = new LoggedInView(loggedInViewModel);
+            applyFontToComponent(loggedInView, goblinFont);
+            views.add(loggedInView, loggedInView.viewName);
+
+            // Activate the initial view
+            viewManagerModel.setActiveView(signupView.viewName);
+            viewManagerModel.firePropertyChanged();
+
+            application.pack();
+            application.setVisible(true);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+            // Handle font loading error
         }
+    }
 
-        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel, signupViewModel, userDataAccessObject);
-        views.add(signupView, signupView.viewName);
-
-        LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel, loggedInViewModel, signupViewModel, userDataAccessObject); // Updated line
-        views.add(loginView, loginView.viewName);
-
-        LoggedInView loggedInView = new LoggedInView(loggedInViewModel);
-        views.add(loggedInView, loggedInView.viewName);
-
-        viewManagerModel.setActiveView(signupView.viewName);
-        viewManagerModel.firePropertyChanged();
-
-        application.pack();
-        application.setVisible(true);
+    private static void applyFontToComponent(Component component, Font font) {
+        component.setFont(font);
+        if (component instanceof Container) {
+            for (Component child : ((Container) component).getComponents()) {
+                applyFontToComponent(child, font);
+            }
+        }
     }
 }
--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -4,129 +4,200 @@ import interface_adapter.signup.SignupController;
 import interface_adapter.signup.SignupState;
 import interface_adapter.signup.SignupViewModel;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.Objects;
-
+import java.io.File;
+import java.io.IOException;
 public class SignupView extends JPanel implements ActionListener, PropertyChangeListener {
+    // Constants
     public final String viewName = "sign up";
 
+    // Controllers and view models
+    private final SignupController signupController;
     private final SignupViewModel signupViewModel;
+
+    // UI components
     private final JTextField usernameInputField = new JTextField(15);
     private final JPasswordField passwordInputField = new JPasswordField(15);
     private final JPasswordField repeatPasswordInputField = new JPasswordField(15);
-    private final SignupController signupController;
-
     private final JButton signUp;
     private final JButton cancel;
     private final JButton skipToLogin;
     private final JLabel imageLabel;
+    private Clip audioClip; // Clip for playing audio
+    private boolean isMuted = false; // Mute state
 
-
+    // Constructor
     public SignupView(SignupController controller, SignupViewModel signupViewModel) {
-        ImageIcon imageIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/AA.jpg"))); // Replace with your image path
-        imageLabel = new JLabel(imageIcon);
+        this.signupController = controller; // Assign the controller
+        this.signupViewModel = signupViewModel; // Assign the view model
+        this.signupViewModel.addPropertyChangeListener(this); // Add this view as a listener to the view model
+        this.add(createMuteButton());
+        // Image setup
+        ImageIcon imageIcon = new ImageIcon(getClass().getResource("/AA.jpg")); // Load the image
+        imageLabel = new JLabel(imageIcon); // Create a label for the image
+        imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the image label
 
-        // Set the alignment of the image label to center
-        imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        this.add(imageLabel);
-        this.signupController = controller;
-        this.signupViewModel = signupViewModel;
-        signupViewModel.addPropertyChangeListener(this);
 
-        JLabel title = new JLabel(SignupViewModel.TITLE_LABEL);
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        LabelTextPanel usernameInfo = new LabelTextPanel(
-                new JLabel(SignupViewModel.USERNAME_LABEL), usernameInputField);
-        LabelTextPanel passwordInfo = new LabelTextPanel(
-                new JLabel(signupViewModel.PASSWORD_LABEL), passwordInputField);
-        LabelTextPanel repeatPasswordInfo = new LabelTextPanel(
-                new JLabel(signupViewModel.REPEAT_PASSWORD_LABEL), repeatPasswordInputField);
 
-        JPanel buttons = new JPanel();
-        signUp = new JButton(signupViewModel.SIGNUP_BUTTON_LABEL);
-        buttons.add(signUp);
-        cancel = new JButton(signupViewModel.CANCEL_BUTTON_LABEL);
-        buttons.add(cancel);
-        skipToLogin = new JButton("Skip to Login");
-        buttons.add(skipToLogin);
+        // Title setup
+        JLabel title = new JLabel(SignupViewModel.TITLE_LABEL); // Create title label
+        title.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the title label
 
-        signUp.addActionListener(
-                // This creates an anonymous subclass of ActionListener and instantiates it.
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(signUp)) {
-                            signupController.execute(usernameInputField.getText(),
-                                    String.valueOf(passwordInputField.getPassword()),
-                                    String.valueOf(repeatPasswordInputField.getPassword()));
-                        }
-                    }
-                }
-        );
+        // Setup for username, password, and repeat password fields
+        LabelTextPanel usernameInfo = new LabelTextPanel(new JLabel(SignupViewModel.USERNAME_LABEL), usernameInputField);
+        LabelTextPanel passwordInfo = new LabelTextPanel(new JLabel(signupViewModel.PASSWORD_LABEL), passwordInputField);
+        LabelTextPanel repeatPasswordInfo = new LabelTextPanel(new JLabel(signupViewModel.REPEAT_PASSWORD_LABEL), repeatPasswordInputField);
+
+        // Buttons setup
+        signUp = createRainbowButton(signupViewModel.SIGNUP_BUTTON_LABEL); // Create a signup button with rainbow effect
+        cancel = createRainbowButton(signupViewModel.CANCEL_BUTTON_LABEL); // Create a cancel button with rainbow effect
+        skipToLogin = createRainbowButton("Skip to Login"); // Create a skip-to-login button with rainbow effect
+
+        // Panel for buttons
+        JPanel buttons = new JPanel(); // Create a panel to hold buttons
+        buttons.add(signUp); // Add the signup button
+        buttons.add(cancel); // Add the cancel button
+        buttons.add(skipToLogin); // Add the skip-to-login button
+
+        // Add action listeners to buttons
+        signUp.addActionListener(e -> signupController.execute(usernameInputField.getText(), new String(passwordInputField.getPassword()), new String(repeatPasswordInputField.getPassword())));
         cancel.addActionListener(this);
-        skipToLogin.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Call method in SignupPresenter to handle view change
-                signupController.goToLogin();
-            }
-        });
-        usernameInputField.addKeyListener(
-                new KeyListener() {
-                    @Override
-                    public void keyTyped(KeyEvent e) {
-                        SignupState currentState = signupViewModel.getState();
-                        currentState.setUsername(usernameInputField.getText() + e.getKeyChar());
-                        signupViewModel.setState(currentState);
-                    }
+        skipToLogin.addActionListener(e -> signupController.goToLogin());
 
-                    @Override
-                    public void keyPressed(KeyEvent e) {
-                    }
-
-                    @Override
-                    public void keyReleased(KeyEvent e) {
-                    }
-                });
+        // Set the layout of the panel
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
+        // Add components to the panel
+        this.add(imageLabel);
         this.add(title);
         this.add(usernameInfo);
         this.add(passwordInfo);
         this.add(repeatPasswordInfo);
         this.add(buttons);
+
+        // Start playing background music
+        playBackgroundMusic("src/view/power.wav");
     }
 
-    /**
-     * React to a button click that results in evt.
-     */
+    private void playBackgroundMusic(String filePath) {
+        try {
+            // Open an audio input stream
+            File audioFile = new File(filePath);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+            audioClip = AudioSystem.getClip(); // Assign to the member variable
+            audioClip.open(audioInputStream);
+
+            // Start playing the music
+            audioClip.start();
+            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace(); // Handle exceptions
+        }
+    }
+    private JButton createMuteButton() {
+        JButton muteButton = new JButton("Mute");
+        muteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (audioClip != null) {
+                    isMuted = !isMuted; // Toggle mute state
+                    FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);
+                    gainControl.setValue(isMuted ? gainControl.getMinimum() : gainControl.getMaximum()); // Mute or unmute
+                    muteButton.setText(isMuted ? "Unmute" : "Mute"); // Update button text
+                }
+            }
+        });
+        return muteButton;
+    }
+
+
+
+
+    // Method to create a button with a rainbow hover effect
+    private JButton createRainbowButton(String text) {
+        RainbowButton button = new RainbowButton(text);
+        return button;
+    }
+
+    // Action performed method for handling button clicks
+    @Override
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource() == cancel) {
-            // Clear all input fields
+            // Clear all text fields when cancel is clicked
             usernameInputField.setText("");
             passwordInputField.setText("");
             repeatPasswordInputField.setText("");
-
-            // Optionally, reset any error states or messages
+            // Reset any error state in the view model
             SignupState currentState = signupViewModel.getState();
-            currentState.setUsernameError(null); // Assuming there's a method to set an error message
+            currentState.setUsernameError(null);
             signupViewModel.setState(currentState);
         }
     }
 
+    // Property change listener to respond to changes in the view model
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        // Handle state changes, like showing error messages
         SignupState state = (SignupState) evt.getNewValue();
         if (state.getUsernameError() != null) {
             JOptionPane.showMessageDialog(this, state.getUsernameError());
+        }
+    }
+
+    // Main method for testing the view
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Signup View");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new SignupView(null, null)); // Add the SignupView to the frame
+            frame.pack(); // Pack the frame
+            frame.setVisible(true); // Make the frame visible
+        });
+    }
+
+    // Inner class for a JButton with a rainbow hover effect
+    static class RainbowButton extends JButton {
+        private final Timer timer; // Timer for animation
+        private float hue = 0.0f; // Hue value for the color
+
+        public RainbowButton(String text) {
+            super(text);
+            setContentAreaFilled(false);
+            setOpaque(true);
+
+            // Setup timer to change color
+            timer = new Timer(50, e -> {
+                hue += 0.01f; // Increment hue
+                if (hue > 1.0f) {
+                    hue = 0.0f; // Reset hue if it goes beyond 1
+                }
+                setBackground(Color.getHSBColor(hue, 0.7f, 0.7f)); // Set background color based on hue
+            });
+
+            // Add mouse listener for hover effect
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    timer.start(); // Start the timer on mouse enter
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    timer.stop(); // Stop the timer on mouse exit
+                    setBackground(UIManager.getColor("Button.background")); // Reset the background color
+                }
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g); // Paint the component normally
         }
     }
 }
