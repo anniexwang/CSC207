34cd063ab9e147f05112fc998229bb4f5fd7bc4b
amelia-kwan <91842034+amelia-kwan@users.noreply.github.com>
2023-11-27 14:37:55 -0500

    implemented TranslationViewModel

--- src/main/java/interface_adapter/translation/TranslationViewModel.java
+++ src/main/java/interface_adapter/translation/TranslationViewModel.java
@@ -1,4 +1,29 @@
 package interface_adapter.translation;
+import interface_adapter.ViewModel;
 
-public class TranslationViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TranslationViewModel extends ViewModel{
+
+    public static final String TRANSLATE_BUTTON_LABEL= "Translate";
+
+    public static final String SELECT_BUTTON_LABEL = "Select Language";
+
+    private TranslationState state = new TranslationState();
+    public TranslationViewModel() {super("translate");}
+
+    // extend ViewModel (rewrite fireProperty changes)
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public TranslationState getState() {
+        return state;
+    }
 }
