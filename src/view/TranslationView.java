6243e3c126a7e66f2c485f2e9688be7c86cb8a81
amelia-kwan <91842034+amelia-kwan@users.noreply.github.com>
2023-11-27 14:07:10 -0500

    added basic structure for TranslationView

--- src/view/TranslationView.java
+++ src/view/TranslationView.java
@@ -1,4 +1,27 @@
 package view;
+import interface_adapter.translation.TranslationController;
+import interface_adapter.translation.TranslationState;
+import interface_adapter.translation.TranslationViewModel;
 
-public class TranslationView {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.*;
+import java.util.Objects;
+
+public class TranslationView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "translate"; // Name of the view
+
+    // Controllers and View Models
+
+    private final TranslationController translationController; // not yet initialized
+    // UI
+    JLabel translateLabel = new JLabel("Select Language");
+    JTextField translatePhrase = new JTextField("Enter Your Text Here");
+    // array of dropdown languages
+    String[] languages = {"Option 1", "Option 2", "Option 3"};
+    JComboBox<String> comboBox = new JComboBox<>(languages);
+
+    public TranslationView(TranslationController translationController, TranslationViewModel translationViewModel){}
