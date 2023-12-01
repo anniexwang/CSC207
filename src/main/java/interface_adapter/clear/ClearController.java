e4744a00fa8a7678068a70a98dc703eadba4f6d4
amelia-kwan <91842034+amelia-kwan@users.noreply.github.com>
2023-11-30 20:22:30 -0500

    completed Clear Controller

--- src/main/java/interface_adapter/clear/ClearController.java
+++ src/main/java/interface_adapter/clear/ClearController.java
@@ -1,4 +1,21 @@
 package interface_adapter.clear;
 
+import interface_adapter.clear.ClearPresenter;
+import use_case.clear.ClearInputBoundary;
+import use_case.clear.ClearInputData;
+
 public class ClearController {
+    private final ClearInputBoundary clearUseCaseInteractor;
+    private final ClearPresenter clearPresenter;
+
+    public ClearController(ClearInputBoundary clearUseCaseInteractor, ClearPresenter clearPresenter) {
+        this.clearUseCaseInteractor = clearUseCaseInteractor;
+        this.clearPresenter = clearPresenter;
+    }
+
+    public void execute(String username) {
+        // need username to access user and delete user
+        ClearInputData clearInputData = new ClearInputData(username);
+        clearUseCaseInteractor.execute(clearInputData);
+    }
 }
