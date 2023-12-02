ee5323559979d57d0923459878a3873e60710b0d
4-Matt <matthew.lin@outlook.com>
2023-12-01 22:33:26 -0500

    Added second endpoint to retrieve target language.

--- src/use_case/retrieve_translation/RetrieveTranslationInteractor.java
+++ src/use_case/retrieve_translation/RetrieveTranslationInteractor.java
@@ -10,19 +10,21 @@ public class RetrieveTranslationInteractor implements RetrieveTranslationInputBo
     final RetrieveTranslationOutputBoundary userPresenter;
 
     private final String api = "AIzaSyAORNlqu0L0NZBzt-ddgWsSYWYTwEOut-A";
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
 
     public RetrieveTranslationInteractor(UserRetrieveTranslationDataAccessInterface userDataAccessObject,
                                          RetrieveTranslationOutputBoundary userPresenter) {
         this.userDataAccessObject = userDataAccessObject;
         this.userPresenter = userPresenter;
     }
-
+    // set target language to autodetected language. It shouldn't change anything.
     @Override
     public void execute(RetrieveTranslationInputData retrieveTranslationInputData) {
         String text = retrieveTranslationInputData.gettext();
+        String targetLang = translate.detect(text).toString();
         Translate translate = TranslateOptions.newBuilder().setApiKey(api).build().getService();
         Translation translation = translate.translate(retrieveTranslationInputData.gettext(),
-                Translate.TranslateOption.targetLanguage(retrieveTranslationInputData.gettargetlanguage()));
+                Translate.TranslateOption.targetLanguage(targetLang));
         String translatedText = translation.getTranslatedText();
         RetrieveTranslationOutputData retrieveTranslationOutputData = new RetrieveTranslationOutputData(translatedText);
         userPresenter.prepareSuccessView(retrieveTranslationOutputData);
