05d5c74a0651f13bc13016d691752a706226f477
4-Matt <matthew.lin@outlook.com>
2023-12-06 22:07:11 -0500

    Added comments to TranslateInteractor.java

--- src/main/java/use_case/translate/TranslateOutputBoundary.java
+++ src/main/java/use_case/translate/TranslateOutputBoundary.java
@@ -1,5 +1,6 @@
 package use_case.translate;
 
+// Interface for TranslateOutputBoundary
 public interface TranslateOutputBoundary {
     void prepareSuccessView(String translateOutputData);
     void prepareFailView(String error);
