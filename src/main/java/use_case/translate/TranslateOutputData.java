aced7a63fb343552d8fb79151a27847883a3f26f
4-Matt <matthew.lin@outlook.com>
2023-12-06 22:09:58 -0500

    Added comments to TranslateOutputData.java

--- src/main/java/use_case/translate/TranslateOutputData.java
+++ src/main/java/use_case/translate/TranslateOutputData.java
@@ -1,7 +1,9 @@
 package use_case.translate;
 
 public class TranslateOutputData {
+    // The translated String
     private final String translated;
+    // Whether or not Google Translate was able to translate the String.
     private boolean useCaseFailed;
     public TranslateOutputData(String translated, boolean useCaseFailed) {
         this.translated = translated;
