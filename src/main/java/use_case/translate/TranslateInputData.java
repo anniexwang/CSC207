df15164b05dc4fa6f1128996fa2faba6e8b56a84
4-Matt <matthew.lin@outlook.com>
2023-12-06 22:01:23 -0500

    Added comments to TranslateInputData.java

--- src/main/java/use_case/translate/TranslateInputData.java
+++ src/main/java/use_case/translate/TranslateInputData.java
@@ -6,13 +6,15 @@ import com.google.cloud.translate.Translate;
 
 public class TranslateInputData {
     final private String original;
+    // the autodetected language of the String to be translated
     final private String originalLang;
-
+    // the username of the user who made the request
     final private String username;
+    // API Key required for the Google Translate API to function.
     private final String api = "AIzaSyAORNlqu0L0NZBzt-ddgWsSYWYTwEOut-A";
+    // Set up API key
     Translate translate = TranslateOptions.newBuilder().setApiKey(api).build().getService();
     public TranslateInputData(String original, String username) {
-
         this.original = original;
         this.username = username;
         this.originalLang = translate.detect(original).getLanguage();
